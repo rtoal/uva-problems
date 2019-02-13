fcabf66d0ecae1704563d4208f55e57067c0cb87
Ray Toal <rtoal@lmu.edu>
2019-02-13 15:55:15 -0800

    11991

---
+++ 11991.py
@@ -0,0 +1,23 @@
+import sys
+
+
+while True:
+    try:
+        size, queries = map(int, sys.stdin.readline().split())
+        a = list(map(int, sys.stdin.readline().split()))
+
+        g = {}
+
+        for i, x in enumerate(a):
+            if x not in g:
+                g[x] = []
+            g[x].append(i+1)
+
+        for _ in range(queries):
+            k, v = map(int, sys.stdin.readline().split())
+            if len(g[v]) < k:
+                print(0)
+            else:
+                print(g[v][k-1])
+    except:
+       break
