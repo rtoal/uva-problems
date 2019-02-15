9168877bb929492be27f402a7b6f703d125e218b
Ray Toal <rtoal@lmu.edu>
2019-02-15 08:33:24 -0800

    Tidy and expand test cases for 195

--- 195.py
+++ 195.py
@@ -1,9 +1,9 @@
 import sys
 
 # String to array of ints that can be arranged and sorted according to the
-# weird rules of the problem. Interleaves uppers and lowers in ASCII letters.
+# weird rules of the problem. Interleaves upper and lower ASCII letters.
 def encoded(s):
-    return [c*2 if c<92 else c*2-63 for c in bytes(s, "utf-8")]
+    return [c*2 if c<92 else c*2-63 for c in bytes(s, 'utf-8')]
 
 # Encoded array back to string
 def decoded(a):
