17c3a87a5eb8907fdcb2eaf73ad1d8f910faad01
Ray Toal <rtoal@lmu.edu>
2019-02-14 22:54:59 -0800

    Make disjoint set class for 11503

--- 11503.py
+++ 11503.py
@@ -1,33 +1,32 @@
 import sys
 
-# Nodes are triples [data, parent, size]
-
-def add(whole, data):
-    if data not in whole:
-        whole[data] = [data, data, 1]
-
-def find(whole, data):
-    if data != whole[data][1]:
-        whole[data] = find(whole, whole[data][1])
-    return whole[data]
-
-def union(whole, x, y):
-    x_root = find(whole, x)
-    y_root = find(whole, y)
-    if x_root == y_root:
+class DisjointSet():
+    def __init__(self):
+        self.network = {}
+    def add(self, x):
+        if x not in self.network:
+            self.network[x] = [x, None, 1]
+    def union(self, x, y): # return the size of the new group
+        x_root = self.find(x)
+        y_root = self.find(y)
+        if x_root == y_root:
+            return x_root[2]
+        if x_root[2] < y_root[2]:
+            x_root, y_root = y_root, x_root
+        y_root[1] = x_root[0]
+        x_root[2] += y_root[2]
         return x_root[2]
-    if x_root[2] < y_root[2]:
-        x_root, y_root = y_root, x_root
-    y_root[1] = x_root[0]
-    x_root[2] += y_root[2]
-    return x_root[2]
+    def find(self, x):
+        if self.network[x][1] is not None:
+            self.network[x] = self.find(self.network[x][1])
+        return self.network[x]
 
 cases = int(sys.stdin.readline())
 for _ in range(cases):
    num_connections = int(sys.stdin.readline())
-   whole = {}
+   s = DisjointSet()
    for connections in range(num_connections):
        friends = sys.stdin.readline().strip().split()
-       add(whole, friends[0])
-       add(whole, friends[1])
-       print(union(whole, friends[0], friends[1]))
+       s.add(friends[0])
+       s.add(friends[1])
+       print(s.union(friends[0], friends[1]))
